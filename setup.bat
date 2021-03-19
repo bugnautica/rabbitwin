17a771321317b4e6728684b7e67ce56e41d7220a
bugnautica <75381552+bugnautica@users.noreply.github.com>
2021-03-19 12:07:22 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,4 +2,10 @@ pip install art
 pip install mss
 pip install opencv-python
 pip install pygame
+pip install numpy
+pip install socket
+pip install sys
+pip install pickle
+pip install zlib
+pip install argparse
 echo sucess modules loaded
